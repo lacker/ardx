1d3153d6aff25ce8af1fa2b1859e5f25f3b6d50b
Kevin Lacker <lacker@gmail.com>
2016-01-02 14:22:28 -0800

    red n green

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ let board = new five.Board();
 
 board.on("ready", function() {
 
-  let led = new five.Led(13);
+  let green = new five.Led(13);
+  let red = new five.Led(10);
 
-  led.strobe(1000);
+  console.log('strobing');
+  green.strobe(1000);
+  red.strobe(500);
 
-  // Make led available in repl
+  // Make leds available in repl
   this.repl.inject({
-    led: led
+    green: green,
+    red: red,
   });
 
   // try "on", "off", "toggle", "strobe", "stop" (stops strobing)

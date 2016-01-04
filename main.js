fead7e4f01d4ef9118244f46e0290916ab7e1066
Kevin Lacker <lacker@gmail.com>
2016-01-03 20:43:33 -0800

    Add another led

--- main.js
+++ main.js
@@ -8,15 +8,18 @@ board.on("ready", function() {
 
   let green = new five.Led(13);
   let red = new five.Led(10);
+  let big = new five.Led(7);
 
   console.log('strobing');
   green.strobe(1000);
   red.strobe(500);
+  big.strobe(250);
 
   // Make leds available in repl
   this.repl.inject({
     green: green,
     red: red,
+    big: big,
   });
 
   // try "on", "off", "toggle", "strobe", "stop" (stops strobing)

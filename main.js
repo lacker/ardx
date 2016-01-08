177e4604bf53d84c2c0e72d6635858d0e43e1c0c
Kevin Lacker <lacker@gmail.com>
2016-01-07 21:21:08 -0800

    does it work? i duno

--- main.js
+++ main.js
@@ -7,10 +7,16 @@ let board = new five.Board();
 board.on('ready', function() {
 
   let servo = new five.Servo(9);
+  let led = new five.Led(5);
 
   this.repl.inject({
     servo: servo,
+    led: led,
   });
 
-  servo.sweep();
+  setInterval(() => {
+    servo.step(37);
+    led.toggle();
+  }, 1000);
+
 });
